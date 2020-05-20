80cef397e5b6e0ea4bc890e42aacc42a6a6f2f6b
Jinwook Kim <jininja7@gmail.com>
2020-05-20 13:19:29 -0700

    added rest of the attributes into their own methods

--- src/ContributionMetadata.js
+++ src/ContributionMetadata.js
@@ -2,22 +2,175 @@ const axios = require('axios');
 
 class ContributionMetadata {
 
+    /**
+     * Gets the web page response body as a string
+     * @private
+     * @param {string} link Link to your contribution page
+     * @return {string} response data which is a string of the entire web page
+     */
     async getResponseBody(link) {
         return (await axios.get(link)).data;
     }
 
+    /**
+     * Gets how many points is associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of points
+     */
     getPoints(body) {
         let pattern = /((\d|,)+) Points/g;
-        let matches = pattern.exec(body);
+        return this.getMatch(pattern, body);
+    }
 
+    /**
+     * Gets what level you are on your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} your level
+     */
+    getLevel(body) {
+        let pattern = /Level (\d+) Local Guide/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many reviews you have left associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of reviews
+     */
+    getReviews(body) {
+        let pattern = /(\d+) reviews/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many ratings you gave associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of ratings
+     */
+    getRatings(body) {
+        let pattern = /(\d+) ratings/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many questions you left associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of questions
+     */
+    getQuestions(body) {
+        let pattern = /(\d+) answers/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many places you added associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of places added
+     */
+    getPlacesAdded(body) {
+        let pattern = /(\d+) places added/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many edits you made associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of edits
+     */
+    getEdits(body) {
+        let pattern = /(\d+) edits/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many facts you left associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of facts
+     */
+    getFacts(body) {
+        let pattern = /(\d+) facts/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many videos you uploaded associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of videos
+     */
+    getVideos(body) {
+        let pattern = /(\d+) videos/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many Q&As you answered associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of Q&As
+     */
+    getQA(body) {
+        let pattern = /(\d+) Q\\\\u0026A/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many roads you added associated with your Local Guide Profile
+     * @public
+     * @return {string} # of roads
+     */
+    getRoadsAdded(body) {
+        let pattern = /(\d+) roads added/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets how many lists you published associated with your Local Guide Profile
+     * @public
+     * @param {string} body Entire web page body
+     * @return {string} # of published lists
+     */
+    getPublishedLists(body) {
+        let pattern = /(\d+) published lists/g;
+        return this.getMatch(pattern, body);
+    }
+
+    /**
+     * Gets the match result based on the pattern searching through the response body of the web page
+     * @private
+     * @param {RegExp} pattern Pattern used to find matches within the body
+     * @param {string} body Entire web page body
+     * @return {string} first group match result if found, empty string if not found.
+     */
+    getMatch(pattern, body) {
+        let matches = pattern.exec(body);
         return matches.length > 0 ? matches[1] : "";
     }
 
     async getMetadata(link) {
         let body = await this.getResponseBody(link);
         let points = this.getPoints(body);
-
-        return points;
+        let level = this.getLevel(body);
+        let reviews = this.getReviews(body);
+        let ratings = this.getRatings(body);
+        let questions = this.getQuestions(body);
+        let placesAdded = this.getPlacesAdded(body);
+        let edits = this.getEdits(body);
+        let facts = this.getFacts(body);
+        let videos = this.getVideos(body);
+        let QA = this.getQA(body);
+        let roads = this.getRoadsAdded(body);
+        let lists = this.getPublishedLists(body);
+        
+        return reviews;
     }
 }
 
