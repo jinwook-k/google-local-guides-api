8e92b76aae9f2d4c90c23bc688be4690ef9d2841
Jinwook Kim <jininja7@gmail.com>
2020-05-20 10:03:06 -0700

    add function for points

--- src/ContributionMetadata.js
+++ src/ContributionMetadata.js
@@ -6,11 +6,32 @@ class ContributionMetadata {
         return (await axios.get(link)).data;
     }
 
+    getPoints(body) {
+        let pattern = /((\d|,)+) Points/g;
+        let matches = pattern.exec(body);
+
+        return matches.length > 0 ? matches[1] : "";
+    }
+
     async getMetadata(link) {
         let body = await this.getResponseBody(link);
-        return body;
+        let points = this.getPoints(body);
+
+        return points;
     }
 }
 
 module.exports = ContributionMetadata;
+
+async function run() {
+    let link = "https://www.google.com/maps/contrib/107673332497849211058/photos/@47.630274,-122.2373958,12z/data=!4m3!8m2!3m1!1e1";
+    let foo = new ContributionMetadata();
+    let data = foo.getMetadata(link);
+
+    return data;
+}
+
+run().then(data => {
+    console.log('data: ', data);
+})
 
