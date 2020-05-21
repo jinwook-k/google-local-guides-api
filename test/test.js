ed02fe4291054c1e8a389dcd435d7468143f5132
Jinwook Kim <jininja7@gmail.com>
2020-05-20 23:21:20 -0700

    add one test for points

--- test/test.js
+++ test/test.js
@@ -1,18 +1,30 @@
 const expect = require('chai').expect;
 
-require('dotenv').config();
+require('dotenv').config({path: "./.env"});
 const ContributionMetadata = require('../src/ContributionMetadata');
 
-describe('ContributionMetadata Class', async function () {
-    let contributionMetadata = new ContributionMetadata();
-    await contributionMetadata.init(process.env.CONTRIB_LINK);
+describe('ContributionMetadata Class',  () => {
+    let contributionMetadata;
 
-    describe('getPoints', function () {
-        let points = contributionMetadata.getPoints();
-        expect(points).to.be.a('string');
+    describe('getPoints()', () => {
+        let points;
+        let parsedPoints;
 
-        let parsedPoints = parseInt(points);
-        expect(parsedPoints).to.be.a('number', 'if able to parseInt');
-        expect(parsedPoints >= 0).to.equal(true, 'parsedInt should equal to or greater than zero');
-    })
+        beforeEach(async () => {
+            contributionMetadata = new ContributionMetadata();
+            await contributionMetadata.init(process.env.CONTRIB_LINK);
+            points = contributionMetadata.getPoints();
+            parsedPoints = parseInt(points);
+        });
+
+        it('should natively be a string', async () => {
+            expect(points).to.be.a('string');
+        });
+        it('should natively be a string', async () => {
+            expect(parsedPoints).to.be.a('number');
+        });
+        it('parsedInt should equal to or greater than zero', async () => {
+            expect(parsedPoints >= 0).to.equal(true);
+        });
+    });
 });
